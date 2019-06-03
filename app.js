e997c90963c150be5ddc9a8e0e9f0ddb2137b359
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-03 10:56:02 -0700

    Made changes to setup files

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('Connected to MongoDB successfully'))
 	.catch(err => console.log(err));
-const port = process.env.PORT || 5000;
+
 app.get('/', (req, res) => res.send('Eventlight all day'));
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
