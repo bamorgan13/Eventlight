b811812b884eb02573ca2b13344778e78fcb38ed
richierob62 <richierob62@gmail.com>
2019-06-04 00:14:38 -0400

    Fix auth and validation

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const Event = require('./Event')
 const UserSchema = new Schema({
   email: {
     type: String,
-    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     required: true,
     index: true
   },
