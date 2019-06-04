363efa060dc4c728e03171fa9009779a753d6f12
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-04 14:03:35 -0700

    Merge master to navbar_and_splash branch

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -15,10 +15,10 @@ class LoginForm extends React.Component {
     this.renderErrors = this.renderErrors.bind(this)
   }
 
-  // Once the user has been authenticated, redirect to the Tweets page
+  // Once the user has been authenticated, redirect to the home page
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentUser === true) {
-      this.props.history.push('/tweets')
+      this.props.history.push('/')
     }
 
     // Set or clear errors
