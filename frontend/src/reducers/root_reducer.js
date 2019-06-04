8e46ef5d8f092a074ace4b1806184418ec767bf9
richierob62 <richierob62@gmail.com>
2019-06-04 14:24:46 -0400

    Create frontend auth

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import session from './session_api_reducer'
+import errors from './errors_reducer'
 
-const RootReducer = combineReducers({});
+const RootReducer = combineReducers({
+  session,
+  errors
+})
 
-export default RootReducer;
+export default RootReducer
