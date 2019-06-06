7f7d8c39956257b6ca4d992425e36518aa8cf60f
Bryce Morgan <bamorgan13@gmail.com>
2019-06-06 12:39:35 -0400

    Remove keys.js from gitignore

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://Event-Light_Admin_01:RichGordyBryce%5FELA01@eventlight-8eztt.mongodb.net/test?retryWrites=true&w=majority',
-	secretOrKey: 'Br5](;-,Sw_.+uay'
-}
