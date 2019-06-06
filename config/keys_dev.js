aec38787a1670638ffa2a400fc2457c1095ce0cf
Bryce Morgan <bamorgan13@gmail.com>
2019-06-06 12:30:37 -0400

    Set up heroku deployment

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://Event-Light_Admin_01:RichGordyBryce%5FELA01@eventlight-8eztt.mongodb.net/test?retryWrites=true&w=majority',
+	secretOrKey: 'Br5](;-,Sw_.+uay'
+}
