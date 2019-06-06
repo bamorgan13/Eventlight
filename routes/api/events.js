fcbf1b4dc85f1dfd967476fe5299b93c36edfd0d
Bryce Morgan <bamorgan13@gmail.com>
2019-06-05 20:00:23 -0400

    Add filter functionality to events controller get index

--- routes/api/events.js
+++ routes/api/events.js
@@ -11,14 +11,54 @@ const Event = require('../../models/Event')
 const validateEventInput = require('../../validation/events')
 
 router.get('/', (req, res) => {
+	// const queryFilters = {
+	// 	type: 'Festival',
+	// 	category: 'Food & Drink',
+	// 	start_date: 'June 13, 2019 11:13:00',
+	// 	end_date: 'July 26, 2019 11:13:00',
+	// 	city: { city: 'Dover', state: 'DE' }
+	// }
+
+	const queryFilters = req.query
+
+	const populateFilters = {
+		creator: { path: 'creator', select: 'full_name email' },
+		type: { path: 'type' },
+		category: { path: 'category' },
+		city: { path: 'location.city' }
+	}
+
+	if (queryFilters.type) {
+		populateFilters.type.match = { name: queryFilters.type }
+	}
+	if (queryFilters.category) {
+		populateFilters.category.match = { name: queryFilters.category }
+	}
+	if (queryFilters.city) {
+		populateFilters.city.match = { city: queryFilters.city.city, state: queryFilters.city.state }
+	}
+	queryFilters.start_date = queryFilters.start_date ? new Date(queryFilters.start_date) : new Date()
+
+	queryFilters.end_date = queryFilters.end_date
+		? new Date(queryFilters.end_date)
+		: new Date(9999, 11, 31, 23, 59, 59, 999)
+
 	Event.find()
-		.populate('creator')
-		.populate('type')
-		.populate('category')
-		.populate('location.city')
+		.populate(populateFilters.creator)
+		.populate(populateFilters.type)
+		.populate(populateFilters.category)
+		.populate(populateFilters.city)
+		.where('start_date')
+		.gte(queryFilters.start_date)
+		.lte(queryFilters.end_date)
 		.sort({ start_date: 1 })
-		.then(events => res.json(events))
-		.catch(err => res.status(404).json({ noEventsFound: 'No events found' }))
+		.then(events => {
+			events = events.filter(event => event.creator && event.type && event.category && event.location.city)
+			res.json(events)
+		})
+		.catch(err => {
+			res.status(404).json({ noEventsFound: 'No events found' })
+		})
 })
 
 module.exports = router
