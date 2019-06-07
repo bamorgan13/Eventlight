2171d38236249411680f929dfea0a8dcd813906c
richierob62 <richierob62@gmail.com>
2019-06-07 15:33:33 -0400

    Restructure event state

--- frontend/src/reducers/events_reducer.js
+++ frontend/src/reducers/events_reducer.js
@@ -1,19 +1,33 @@
-import { RECEIVE_EVENTS, RECEIVE_EVENT } from '../actions/event_actions'
+import { merge } from 'lodash'
 
-const EventsReducer = (state = {}, action) => {
-	Object.freeze(state)
-	let newState = Object.assign({}, state)
-	switch (action.type) {
-		case RECEIVE_EVENTS:
-			newState = {}
-			action.events.data.forEach(event => (newState[event._id] = event))
-			return newState
-		case RECEIVE_EVENT:
-			newState[action.event.data._id] = action.event.data
-			return newState
-		default:
-			return state
-	}
+import {
+  RECEIVE_EVENTS,
+  RECEIVE_LIKED_EVENTS,
+  RECEIVE_EVENT
+} from '../actions/event_actions'
+
+const initialState = { all: {}, liked_events: {} }
+
+const EventsReducer = (state = initialState, action) => {
+  Object.freeze(state)
+  let newState = merge({}, state)
+  switch (action.type) {
+    case RECEIVE_EVENTS:
+      newState['all'] = {}
+      action.events.data.forEach(event => (newState['all'][event._id] = event))
+      return newState
+    case RECEIVE_EVENT:
+      newState['all'][action.event.data._id] = action.event.data
+      return newState
+    case RECEIVE_LIKED_EVENTS:
+      newState['liked_events'] = {}
+      action.events.data.forEach(
+        event => (newState['liked_events'][event._id] = event)
+      )
+      return newState
+    default:
+      return state
+  }
 }
 
 export default EventsReducer
