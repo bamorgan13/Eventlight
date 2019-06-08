f3a0c6d8e83de5315cef9e1819e0f4551f9895b9
richierob62 <richierob62@gmail.com>
2019-06-07 20:27:59 -0400

    Create reservations/tickets page

--- frontend/src/util/event_api_util.js
+++ frontend/src/util/event_api_util.js
@@ -8,6 +8,10 @@ export const getLikedEvents = () => {
   return axios.get('/api/likes')
 }
 
+export const getRegistrations = () => {
+  return axios.get('/api/registrations')
+}
+
 export const getEvent = _id => {
-	return axios.get('/api/events/:id', { params: { _id } })
+  return axios.get('/api/events/:id', { params: { _id } })
 }
