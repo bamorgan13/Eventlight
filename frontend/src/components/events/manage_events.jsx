fab5255273ca569276ae7acaab8b19e6f1a2f787
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-06-09 15:29:40 -0700

    Adds styling to the manage events page

--- frontend/src/components/events/manage_events.jsx
+++ frontend/src/components/events/manage_events.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/manage_events.css";
 
 class ManageEvents extends React.Component {
   componentWillMount() {
@@ -8,16 +10,28 @@ class ManageEvents extends React.Component {
   render() {
     if (!this.props.currentUsersEvents) return null;
     const currentUserEventItems = Object.values(this.props.currentUsersEvents).map( event => 
-      <li>
-        {event.title}
+      <li key={event._id} className="manage-events-list-item">
+        <div className="manage-events-list-item-info">
+          <h3><span>{event.title}</span></h3>
+          <span>Date here once rich approves pull request</span>
+          <ul className="manage-events-list-item-actions">
+            <li className="manage-events-list-item-actions-icon"><i className="fas fa-cog"></i></li>
+            <li className="manage-events-list-item-actions-text">Manage</li>
+            <li className="manage-events-list-item-actions-icon"><i className="fas fa-pencil-alt"></i></li>
+            <li className="manage-events-list-item-actions-text">Edit</li>
+            <li className="manage-events-list-item-actions-icon"><i className="fas fa-external-link-alt"></i></li>
+            <li className="manage-events-list-item-actions-text"><Link to={`/events/${event._id}`}>View</Link></li>
+          </ul>
+        </div>
+        <div className="manage-events-list-item-capacity">{`0/${event.capacity}`}</div>
       </li>  
     )
 
     return (
-      <div>
+      <div className="manage-events-container">
         <h1>Manage Events</h1>
-        <div>
-          <ul>
+        <div className="manage-events-list-container">
+          <ul className="manage-events-list">
             {currentUserEventItems}
           </ul>
         </div>
