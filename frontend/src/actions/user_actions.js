b3e20034ca42edef8367c54c3912eb9a08ef4208
richierob62 <richierob62@gmail.com>
2019-06-10 17:45:58 -0400

    Create sign in required modal and link to like action

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,9 +1,17 @@
 import * as ApiUtils from '../util/user_api_util'
+import { showSignInErrorModal } from './signin_error_modal_actions'
 
 export const UPDATE_LIKES = 'UPDATE_LIKES'
 
 export const toggleLikeEvent = event_id => (dispatch, getState) => {
-  if (!getState().session.user) return
+  if (!getState().session.isAuthenticated) {
+    dispatch(
+      showSignInErrorModal(
+        "You'll have to sign in or register before you can like events"
+      )
+    )
+    return
+  }
   return ApiUtils.toggleLikeEvent(event_id).then(
     ({ data: { liked_events } }) => {
       dispatch({
