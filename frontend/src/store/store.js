8310836c2f628debc42b6f918e6f76b02fc4c29d
Gordy Cooper <gcooper7@mail.csuchico.edu>
2019-07-30 16:22:48 -0700

    Removes Redux logger in production

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -6,11 +6,19 @@ import rootReducer from '../reducers/root_reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const configureStore = (preloadedState = {}) =>
-  createStore(
-    rootReducer,
-    preloadedState,
-    composeEnhancers(applyMiddleware(thunk, logger))
-  )
-
+const configureStore = (preloadedState = {}) => {
+  if (process.env.NODE_ENV === 'production') {
+    createStore(
+      rootReducer,
+      preloadedState,
+      composeEnhancers(applyMiddleware(thunk))
+    )
+  } else {
+      createStore(
+        rootReducer,
+        preloadedState,
+        composeEnhancers(applyMiddleware(thunk, logger))
+      )
+  }
+}
 export default configureStore
