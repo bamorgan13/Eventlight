04cc26c4e570e63ba5d6771d5947618dc207d23f
Bryce Morgan <bamorgan13@gmail.com>
2019-11-04 14:13:52 -0500

    Add dockerfile and docker-compose

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
-import splashImage from "./bg-desktop-snowglobe.jpg";
-import SearchBar from "../search/search_bar_container";
-import LikeIndexContainer from "../likes/like_index_container";
-import EventIndexContainer from "../events/event_index_container";
-import "../../styles/splash.css";
+import splashImage from './bg-desktop-snowglobe.jpg';
+import SearchBar from '../search/search_bar_container';
+import LikeIndexContainer from '../likes/like_index_container';
+import EventIndexContainer from '../events/event_index_container';
+import '../../styles/splash.css';
+import axios from 'axios';
 
 class MainPage extends React.Component {
-  componentWillMount() {
-    this.props.fetchEvents();
-  }
+	constructor(props) {
+		super(props);
+		this.state = { splash: '#' };
+	}
+	componentWillMount() {
+		this.props.fetchEvents();
+		// axios.get('/static/bg-desktop-snowglobe.jpg').then(img => {
+		// 	debugger;
+		// 	this.setState({ splash: img.config.url });
+		// });
+	}
 
-  componentWillUnmount() {
+	componentWillUnmount() {
 		this.props.clearAutocomplete();
-  }
+	}
 
-  render() {
-    const likesIndexEle = this.props.loggedIn ? <LikeIndexContainer /> : null;
+	render() {
+		const likesIndexEle = this.props.loggedIn ? <LikeIndexContainer /> : null;
 
-    return (
-      <div className="splash-page">
-        <div className="splash-header">
-          <div className="splash-header-image">
-            <img className="splash-header-image" src={splashImage}/>
-          </div>
-          <div className="splash-header-search">
-          <SearchBar classPrefix="splash"/>
-          </div>
-        </div>
-        <div className="splash-likes-container">
-          {likesIndexEle}
-        </div>
-        <div className="splash-events-background">
-          <div className="splash-events-container">
-            <h1>Live your best life</h1>
-            <EventIndexContainer />
-          </div>
-        </div>
-        <footer>
-          <h1>Eventlight</h1>
-          <div>Copyright &copy; 2019 BryGorRic</div>
-        </footer>
-      </div>
-    )
-  }
+		return (
+			<div className="splash-page">
+				<div className="splash-header">
+					<div className="splash-header-image">
+						<img className="splash-header-image" src="/static/bg-desktop-snowglobe.jpg" alt="splash" />
+					</div>
+					<div className="splash-header-search">
+						<SearchBar classPrefix="splash" />
+					</div>
+				</div>
+				<div className="splash-likes-container">{likesIndexEle}</div>
+				<div className="splash-events-background">
+					<div className="splash-events-container">
+						<h1>Live your best life</h1>
+						<EventIndexContainer />
+					</div>
+				</div>
+				<footer>
+					<h1>Eventlight</h1>
+					<div>Copyright &copy; 2019 BryGorRic</div>
+				</footer>
+			</div>
+		);
+	}
 }
 
-export default MainPage
+export default MainPage;
--- routes/api/registrations.js
+++ routes/api/registrations.js
@@ -1,44 +1,65 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-const Event = require('../../models/Event')
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const Event = require('../../models/Event');
+const User = require('../../models/User');
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 	if (!req.user) {
-		res.json({})
+		res.json({});
 	} else {
 		Event.find()
 			.where('_id')
 			.in(req.user.registrations)
 			.sort({ start_date: 1 })
-			.then(events => res.json(events))
+			.then(events => res.json(events));
 	}
-})
+});
 
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-	const user = req.user
-	const idx = user.registrations.indexOf(req.body.event_id)
+// router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+// 	const user = req.user;
+// 	const idx = user.registrations.indexOf(req.body.event_id);
+// 	let payload = {};
+// 	Event.findById(req.body.event_id).then(event => {
+// 		const eIdx = event.attendees.indexOf(user._id);
+// 		if (idx < 0) {
+// 			event.attendees.push(user._id);
+// 		} else {
+// 			event.attendees.splice(eIdx, 1);
+// 		}
+// 		event.save().then(event => {
+// 			payload['attendees'] = event.attendees;
+// 		});
+// 	});
+
+// 	if (idx < 0) {
+// 		user.registrations.push(req.body.event_id);
+// 	} else {
+// 		user.registrations.splice(idx, 1);
+// 	}
+// 	user.save().then(user => {
+// 		payload['registrations'] = user.registrations;
+// 		return res.json(payload);
+// 	});
+// });
+
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+	const userId = req.user.id;
+	const eventId = req.body.event_id;
 	let payload = {};
-	Event.findById(req.body.event_id)
-		.then( event => {
-			const eIdx = event.attendees.indexOf(user._id)
-			if (idx < 0) {
-				event.attendees.push(user._id)
-			}
-			event.save().then(event => {
-				payload["attendees"] = event.attendees
-			})
-		})
+	let updatedEvent = await Event.findOneAndUpdate(
+		{ _id: eventId },
+		{ $addToSet: { attendees: userId } },
+		{ new: true }
+	);
+	let updatedUser = await User.findOneAndUpdate(
+		{ _id: userId },
+		{ $addToSet: { registrations: eventId } },
+		{ new: true }
+	);
+	payload.attendees = updatedEvent.attendees;
+	payload.registrations = updatedUser.registrations;
+	return res.json(payload);
+});
 
-	if (idx < 0) {
-		user.registrations.push(req.body.event_id)
-	}
-	user.save().then(user => {
-		payload["registrations"] = user.registrations
-		return res.json(payload)
-	})
-
-	
-})
-
-module.exports = router
+module.exports = router;
